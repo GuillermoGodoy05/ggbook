29fe5f29dae893446d7def79adba1dfeae827b0c
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-05-29 01:29:32 -0300

    avance

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   imports: [
-   
+    MatGridListModule,
+    CommonModule,
+    MatCardModule,
+    MatIconModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  technologies = [
+    { name: 'C#' },
+    { name: 'Angular' },
+    { name: 'React' },
+    { name: 'Kotlin' },
+    { name: 'Git' },
+    { name: 'Java' },
+    { name: 'JavaScript' },
+    { name: 'TypeScript' },
+    { name: 'SQL Server' },
+    { name: '.NET Core' },
+    { name: 'MERN Stack' },
+    { name: 'Azure DevOps' }
+  ];
 
 }
--- src/app/Pages/projects/projects.component.ts
+++ src/app/Pages/projects/projects.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatGridListModule,
+    CommonModule
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+
+
+  projects = [
+    { name: 'C#' },
+    { name: 'Angular' },
+    { name: 'React' },
+    { name: 'Kotlin' },
+
+  ];
+
+  /* {
+  name: 'Mi Proyecto Angular',
+    imageUrl: 'assets/img/angular-proyecto.png'
+  } */
 
 }
