c646b0402c622b014f81c65c17419b99901c24cc
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-05-29 22:09:51 -0300

    Lista la base de las cards

--- src/app/Pages/projects/projects.component.ts
+++ src/app/Pages/projects/projects.component.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @Component({
   selector: 'app-projects',
   imports: [
+    CommonModule,
     MatCardModule,
     MatGridListModule,
-    CommonModule
+    MatIconModule
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
@@ -20,16 +22,45 @@ export class ProjectsComponent {
 
 
   projects = [
-    { name: 'C#' },
-    { name: 'Angular' },
+    {
+      name: 'My Finance',
+      description: 'finance',
+
+      tech: ['.Net', 'React'],
+
+      label2: 'Try',
+      icon2: 'computer',
+      link2: 'https://matesoft-myfinances.netlify.app/',
+
+      label3: 'Git',
+      icon3: 'code',
+      link3: 'https://github.com/GuillermoGodoy05',
+
+    },
+    {
+      name: 'API Sistema Ventas',
+      description: 'Api Sist Venta',
+
+      tech: ['.Net', 'React'],
+
+      label2: 'Try',
+      icon2: 'computer',
+      link2: 'https://matesoft-myfinances.netlify.app/',
+
+      label3: 'Back',
+      icon3: 'code',
+      link3: 'https://github.com/GuillermoGodoy05/BackendSistemaVenta',
+
+      labelOp: 'Front',
+      iconOp: 'brush',
+      linkOp: 'https://github.com/GuillermoGodoy05/FrontendSistemaVentas',
+
+
+    },
     { name: 'React' },
     { name: 'Kotlin' },
 
   ];
 
-  /* {
-  name: 'Mi Proyecto Angular',
-    imageUrl: 'assets/img/angular-proyecto.png'
-  } */
 
 }
