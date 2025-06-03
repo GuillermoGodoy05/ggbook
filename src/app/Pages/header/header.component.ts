377bd69616a6c5b0f5cc2ca2f7ee2d073c44cec8
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-06-03 15:44:48 -0300

    listo mq header

--- src/app/Pages/header/header.component.ts
+++ src/app/Pages/header/header.component.ts
@@ -1,12 +1,7 @@
 import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
 import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -15,12 +10,10 @@ import { MatButtonModule } from '@angular/material/button';
   standalone: true, // necesario si estás usando componentes independientes (Angular >= 14)
   imports: [
     CommonModule,
-    RouterOutlet,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule,
-    MatListModule
+
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
@@ -29,17 +22,9 @@ export class HeaderComponent {
   // Evento que emite el id de la sección a la que se debe hacer scroll
   @Output() scrollToSection = new EventEmitter<string>();
 
-
   // Modo oscuro activado o no
   isDarkMode = false;
 
-  // ViewChild nos da acceso directo al sidenav desde el template
-  @ViewChild('sidenav') sidenav!: MatSidenav;
-
-  // Alterna entre abrir y cerrar el menú hamburguesa
-  toggleSidenav(): void {
-    this.sidenav.toggle();
-  }
 
   // Emite el evento con la sección correspondiente
   onClick(sectionId: string): void {
@@ -54,5 +39,5 @@ export class HeaderComponent {
     body.classList.toggle('light-theme', !this.isDarkMode);
   }
 
-  
+
 }
